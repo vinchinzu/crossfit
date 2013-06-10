562b33ae65019a9007ce7ac0d137a6df23470338
vinchinzu <lukevinze@gmail.com>
2013-06-09 22:38:12 -0500

    parse data layout

--- scrape1.rb
+++ scrape1.rb
@@ -1,15 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
 
+athlete = [34796,47661,163784]
+url= "http://games.crossfit.com/athlete/"
 
+output = File.open( "data.yml", "w" )
 
-ATHLETE_NUM = 11435
-BASE_URL = "http://games.crossfit.com/athlete/"
+athlete.each do |num|
 
-LIST_URL = "#{BASE_URL}#{ATHLETE_NUM}"
-doc = Nokogiri::HTML(open(LIST_URL))
+list = "#{url}#{num}"
+doc = Nokogiri::HTML(open(list))
 #rows = page.css('td')
 
-doc.css('td').each do |x|
-puts x.content
+doc.css('#page-title').each do |x|
+   puts x.content
+   output << x.content
+end
+
+doc.css('dd').each{|x| output<< x.content}
+
+doc.css('td').each{|x| output<< x.content}
+
+doc.css('div.content.clearfix h4').each{|x| output<< x.content}
+
+
 end
